de999bd5f3ba1addbb9fede01fcaac03626092cf
reactVishnu <thirdparties47@gmail.com>
2023-11-01 18:27:24 +0530

    Custom User Model Added

---
+++ user_app/models.py
@@ -0,0 +1,28 @@
+"""
+Database Models
+"""
+
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
+
+# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class CustomUserModel(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
