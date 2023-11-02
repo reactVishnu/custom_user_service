f72c0fe2ff193301f0c64c4358d5cf81df3a2bdc
reactVishnu <thirdparties47@gmail.com>
2023-11-02 18:58:22 +0530

    Test for User API added

--- user_app/tests/test_user.py
+++ user_app/tests/test_user.py
@@ -41,4 +41,14 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_
+    def test_password_too_short_error(self):
+        """Testing an error if password is less than 8 characters."""
+        payload = {
+            "email": "test@gmail.com",
+            "password": "123",
+            "name": "Test Name"
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        self.assertFalse(user_exists)
